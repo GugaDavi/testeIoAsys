9765f06a8c2afb092c0bcbf74b7ff5e1f169f58f
GugaDavi <gguedes.tech@gmail.com>
2019-09-15 22:11:04 -0300

    Enterprise Global State

---
+++ src/store/modules/enterprise/saga.js
@@ -0,0 +1,20 @@
+import { all, put, call, takeLatest } from 'redux-saga/effects';
+import { Alert } from 'react-native';
+
+import api from '../../../services/api';
+
+import { enterprisesSuccess } from './actions';
+
+export function* loadEnterprises() {
+  try {
+    const response = yield call(api.get, '/api/v1/enterprises');
+
+    const { enterprises } = response.data;
+
+    yield put(enterprisesSuccess(enterprises));
+  } catch (error) {
+    Alert.alert('Erro ao carregar os dados', 'Tente novamente mais tarde');
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_SUCCESS', loadEnterprises)]);
